29f4f0d567e3e478e6295a0be7db8ea4f1892aa1
soonghee2 <soonghee2@ajou.ac.kr>
2025-02-13 10:23:01 +0900

    Create version.c

---
+++ version.c
@@ -0,0 +1,25 @@
+//송희 과제 때문에 잠시만 추가하겠습니다..
+
+#include <stdio.h>
+
+#ifdef linux
+int JenkinsBuilderFileVersion[4] __attribute__((section ("FileVersion"))) = { 2,0,2,51 };
+int JenkinsBuilderProductVersion[4] __attribute__((section ("ProductVersion"))) = FILE_VERSION;
+#endif
+
+int main(int nArgc, char** pszArgv)
+{
+    printf("FileVersion: %d.%d.%d.%d\n",
+        JenkinsBuilderFileVersion[0],
+        JenkinsBuilderFileVersion[1],
+        JenkinsBuilderFileVersion[2],
+        JenkinsBuilderFileVersion[3]
+        );
+    printf("ProductVersion: %d.%d.%d.%d\n",
+        JenkinsBuilderProductVersion[0],
+        JenkinsBuilderProductVersion[1],
+        JenkinsBuilderProductVersion[2],
+        JenkinsBuilderProductVersion[3]
+        );
+    return 0;
+}
